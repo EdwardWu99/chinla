412d76c86e6bc4c8f1ded921591b568ba644c625
Edward <wufei420892457@gmail.com>
2024-12-27 22:21:13 +0000

    console log has no permission

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
-// $(".row").hide();
-// $(".adsbygoogle").hide();
-// $(".ads_right3").hide();
-// $(".post_ads").hide();
-// $(".weixin_ads_app").hide();
-// $(".tbl-next-up").hide();
+// hide all ads
+$(".row").hide();
+$(".adsbygoogle").hide();
+$(".ads_right3").hide();
+$(".post_ads").hide();
+$(".weixin_ads_app").hide();
+$(".tbl-next-up").hide();
 
-console.log('hide all ads')
