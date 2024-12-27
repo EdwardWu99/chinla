a3ebabacc9c4c8d23cf4975ff8477ac89d3fe127
Edward <wufei420892457@gmail.com>
2024-12-27 22:10:25 +0000

    test

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
-$(".row").hide();
-$(".adsbygoogle").hide();
-$(".ads_right3").hide();
-$(".post_ads").hide();
-$(".weixin_ads_app").hide();
-$(".tbl-next-up").hide();
+// $(".row").hide();
+// $(".adsbygoogle").hide();
+// $(".ads_right3").hide();
+// $(".post_ads").hide();
+// $(".weixin_ads_app").hide();
+// $(".tbl-next-up").hide();
 
 console.log('hide all ads')
